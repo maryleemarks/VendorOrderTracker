f0a6926219ed8b3e97f0372a4854a5621eedf212
Mary Marks <maryleemarks@gmail.com>
2021-09-23 12:11:48 -0700

    pass fourth vendor test

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -8,11 +8,13 @@ namespace VendorOrderTracker.Models
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Id { get; }
     public Vendor(string name, string vendorDescription)
     {
       Name = name;
       Description = vendorDescription;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static void ClearAll()
@@ -22,6 +24,10 @@ namespace VendorOrderTracker.Models
     public static List<Vendor> GetAll()
     {
       return _instances;
+    }
+    public static Vendor find(int searchId)
+    {
+      return _instances[searchId-1];
     }
 
 
