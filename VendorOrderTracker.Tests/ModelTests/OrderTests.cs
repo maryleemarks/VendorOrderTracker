9dc1b3640a269684f80575260f5cfe70b9c946b5
Mary Marks <maryleemarks@gmail.com>
2021-09-23 10:17:33 -0700

    set up for testing

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+using System;
+
+namespace VendorOrderTracker.Tests 
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+  }
+}
---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
+using System;
+
+namespace VendorOrderTracker.Tests 
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+  }
+}
