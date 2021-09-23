97b3945367b99232e5e46e0112262f8e15761af3
Mary Marks <maryleemarks@gmail.com>
2021-09-23 10:49:24 -0700

    pass third order test

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -31,5 +31,16 @@ namespace VendorOrderTracker.Tests
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
     }
+    [TestMethod]
+    public void GetDescription_ReturnsDescriptionString()
+    {
+      string title = "Allen Reg";
+      string description = "10 loaves";
+      int price = 35;
+      DateTime date = DateTime.Today;
+      Order newOrder = new Order(title, description, price, date);
+      string result = newOrder.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
