64bc7efdbca39bc66da9625b62c36fdb2935c6b7
Mary Marks <maryleemarks@gmail.com>
2021-09-23 12:30:50 -0700

    passes sisxth vendor test

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -25,7 +25,7 @@ namespace VendorOrderTracker.Models
     {
       return _instances;
     }
-    public static Vendor find(int searchId)
+    public static Vendor Find(int searchId)
     {
       return _instances[searchId-1];
     }
