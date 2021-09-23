f9d0bf3b22fafdeb37b93050706fefcd5e71b534
Mary Marks <maryleemarks@gmail.com>
2021-09-23 12:45:44 -0700

    update orders controller

---
+++ VendorOrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class OrderdController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+  }
+}
