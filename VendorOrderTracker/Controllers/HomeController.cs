c161a6071e3de251f3a5e745e75057eacf6349e5
Mary Marks <maryleemarks@gmail.com>
2021-09-23 09:48:44 -0700

    add code to oreder model and initial code to home controller

---
+++ VendorOrderTracker/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+  }
+}
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -12,7 +12,30 @@ namespace VendorOrderTracker.Models
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
-    
+    public Order(string title, string description, int price, DateTime date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = DateTime.Today;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
 
